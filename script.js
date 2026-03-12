291e7afa4ffead493169db060d2075d12ce3bce8
Mikey <michaeldedeji@gmail.com>
2026-03-12 16:06:08 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+const messages = [
+"boba!!! 🧋",
+"hehe",
+"오늘 boba party <3",
+"bubble tea time",
+"와",
+"hi bunnies",
+"newjeans forever",
+"who wants boba?"
+];
 
+function spawnMessage(){
+  const msg = document.createElement("div");
+  msg.className = "random-message";
+
+  msg.innerText = messages[Math.floor(Math.random()*messages.length)];
+
+  msg.style.left = Math.random()*80 + "%";
+  msg.style.top = Math.random()*80 + "%";
+
+  document.body.appendChild(msg);
+
+  setTimeout(()=>msg.remove(),4000);
+}
+
+setInterval(spawnMessage,3000);
